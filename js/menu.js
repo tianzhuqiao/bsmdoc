51e2d7046e5b0936464f3df96efd14ffe1b90d0d
Tianzhu Qiao <tq@feiyilin.com>
2018-09-09 21:52:53 -0700

    Update doc.

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ $(window).load(function() {
 var lastId,
     topMenu = $(".menu"),
     topTitleHeight = $(".toptitle").outerHeight()+15,
+    topUL = topMenu.find('ul'),
     // All list items
     menuItems = topMenu.find("a"),
     // Anchors corresponding to menu items
@@ -11,17 +12,20 @@ var lastId,
         if (item.length) { return item; }
     });
 
+var auto_hide_child_menu = menuItems.length > 15;
 // Bind click handler to menu items so we can get a fancy scroll animation
-menuItems.click(function(e){
-    var href = $(this).attr("href"),
-        offsetTop = href === "#" ? 0 : $(href).offset().top-15+1;
-    $('html, body').stop().animate({
-        scrollTop: offsetTop
-    }, 300);
-    e.preventDefault();
-});
-// Bind to scroll
-$(window).scroll(function(){
+if (!auto_hide_child_menu) {
+    menuItems.click(function(e){
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top-15+1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
+    });
+}
+
+function update_menu() {
     // Get container scroll position
     var fromTop = $(this).scrollTop() + 15;
 
@@ -39,6 +43,21 @@ $(window).scroll(function(){
         // Set/remove active class
         menuItems.removeClass("active");
         menuItems.filter("[href='#"+id+"']").addClass("active");
+        if (auto_hide_child_menu) {
+            topUL.find('ul').each(function(index){
+                $(this).css('display', 'none');
+            });
+            menu = menuItems.filter("[href='#"+id+"']");
+            menuul = menu.closest('ul').css('display', 'inline-block');
+            menu.closest('li').children('ul').css('display', 'inline-block');
+        }
     }
+}
+
+// Bind to scroll
+$(window).scroll(function(){
+    update_menu();
 });
+
+update_menu();
 });
