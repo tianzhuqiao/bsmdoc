ef5c99799f7db156e01f0b8a59154cd21889985a
Tianzhu Qiao <tq@feiyilin.com>
2020-07-14 22:37:30 -0700

    version 0.0.7

--- bsmdoc.py
+++ bsmdoc.py
@@ -14,7 +14,7 @@ from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 
 class BConfig(object):
